1922881b25b6b6abf3ebd486e27510c7c797c2fd
Martin Bieker <martin.bieker@udo.edu>
2014-04-21 21:15:10 +0200

    update

--- beta.py
+++ beta.py
@@ -3,6 +3,7 @@ from uncertainties import  ufloat
 from matplotlib.pyplot import *
 
 from Tools import lin_reg , make_LaTeX_table
+from sympy.physics.units import length
 data = genfromtxt("beta_data1.csv", delimiter=',', unpack="true")
 
 N_3 = data[1]
@@ -25,16 +26,38 @@ A_3_err = array([fehler(i,N_0,t_3,t_0) for i in N_3])
 m_1,b_1 = lin_reg(d_3[:12],log(A_3[:12]))
 m_2,b_2 = lin_reg(d_3[13:], log(A_3[13:]))
 
+
+print " m_1: %s b_1:%s" % (m_1,b_1)
+print " m_2: %s b_2:%s" % (m_2,b_2)
 x_1 = linspace(0,1e-3)
 x_2 = linspace(0,2500e-6)
 
+R_max = (b_2-b_1)/(m_1-m_2)
 
+print "R_Max: %s mm " % (R_max*1e3)
 
+E_Max = 1.92*(R_max**2+0.22*R_max)**0.5 
+
+print "E_max : %s MeV" % E_Max
 xlabel("Schichtdicke [$\mu m$]")
 ylabel("$A$ in [$s^{-1}$]")
 
-errorbar(d_3*1e6, A_3, A_3_err,0,'.')
-plot(x_1*1e6,exp( m_1.n*x_1+b_1.n))
-plot(x_2*1e6, exp(m_2.n*x_2+b_2.n))
+errorbar(d_3*1e6, A_3, A_3_err,0,'.', label= "Messwerte")
+plot(x_1*1e6,exp( m_1.n*x_1+b_1.n), label= "Lineare Regession Teil 1")
+plot(x_2*1e6, exp(m_2.n*x_2+b_2.n), label= "Lineare Regession Teil 2")
+
+plot([R_max.n*1e6], [exp(m_1.n*R_max.n +b_1.n)],'ro', )
 yscale("log")
-show()
+
+legend()
+
+savefig("beta_log.png")
+
+
+# Tabelle erstellen
+
+data = [[int(d_3[i]*1e6),int(N_3[i]), ufloat(A_3[i], A_3_err[i])] for i in range(len(d_3))]
+
+header = [ r"$\frac{d}{\si{\micro\meter}}$",r"$N$" , r"$\frac{A}{\si{\second^{-1}}}$"]
+
+print make_LaTeX_table(array(data), header)
--- gamma.py
+++ gamma.py
@@ -1,5 +1,6 @@
 from numpy import genfromtxt, sqrt, array, log, linspace, exp, pi
 from uncertainties import ufloat
+from scipy.constants import *
 from matplotlib.pyplot import *
 data = genfromtxt("gamma_data1.csv", delimiter=',', unpack="true")
 
@@ -16,11 +17,10 @@ def fehler(N,N_0,t,t_0):
 
 def mu_theo(Z, M, rho):
     r_e = 2.82e-15
-    E_gamma = 1
-    m_0  = 1
-    N_L = 1
-    eps = E_gamma/(3e8**2*m_0)
-    sigma = 2 * pi * r_e**2 ((1+eps)/eps**2 *(2*(1+eps)/(1+2*eps)-1/eps*log(1+2*eps)+0.5/eps*log(1+2*eps)- (1+3*eps)/(1+2*eps)**2))
+    E_gamma = 667e3 * electron_volt
+    N_L = N_A
+    eps = E_gamma/(c**2*m_e)
+    sigma = 2 * pi * r_e**2 *((1+eps)/eps**2 *(2*(1+eps)/(1+2*eps)-1/eps*log(1+2*eps)+0.5/eps*log(1+2*eps)- (1+3*eps)/(1+2*eps)**2))
     return Z*N_L*rho*sigma/M
 t_0 = 900.0 
 t_1 = 300.0
@@ -64,3 +64,15 @@ savefig("gamma1_log.png")
 
 data = array([[int(d_1[i]*1e3),int(N_1[i]), ufloat(A_1[i]-A_0, A_1_err[i]) ]for i in range(7)])
 print make_LaTeX_table(data,  [r'{$\frac{d}{\si{\milli\meter}}$} ',r'$N$' ,r'{ $\frac{A-A_0}{\si{\second^{-1}}}$ }'])
+
+
+
+#Theorie vgl.
+
+mu_th  = mu_theo(29, 63.546, 8.92e3)
+print "mu_theo_cu: %s " % mu_th
+
+
+
+# Das Gleiche nochmal für Pb
+
